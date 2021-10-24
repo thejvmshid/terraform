0c4511702e186fe6b0610dea4cebae8699ba582c
Jamshid Tursunov <fj28s@Jamshids-MacBook-Pro.local>
2021-10-25 00:58:01 +0500

    Initial commit

--- main.tf
+++ main.tf
@@ -1,7 +1,38 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
 resource "aws_instance" "example" {
+  vpc_security_group_ids = [aws_security_group.instance.id]
   ami = "ami-0a49b025fffbbdac6"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
